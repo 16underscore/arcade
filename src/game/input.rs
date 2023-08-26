9a9fbc590b437c7bcc73536b67bc3fdc9082ff1f
16_ <45466314+16underscore@users.noreply.github.com>
2023-08-26 14:04:39 +0200

    remove gamepad support

--- src/game/input.rs
+++ src/game/input.rs
@@ -12,17 +12,15 @@ impl Plugin for InputPlugin {
 }
 
 fn input(
-	gamepads: Res<Gamepads>,
-	axes: Res<Axis<GamepadAxis>>,
 	windows: Query<&Window, With<PrimaryWindow>>,
 	keyboard: Res<Input<KeyCode>>,
 	mut players: Query<(&Speed, &mut Velocity, &mut Transform), With<Player>>,
 ) {
 	let window = windows.single();
-	let (speed, mut velocity, mut transform) = players.single_mut();
 	if let Some(position) = window.cursor_position() {
 		let x = position.x - window.width() / 2.;
 		let z = position.y - window.height() / 2.;
+		let (speed, mut velocity, mut transform) = players.single_mut();
 		let direction = Vec3::new(x, 0., z).clamp_length(-speed.0, speed.0);
 		let pos = transform.translation;
 		transform.look_at(pos + direction, Vec3::Y);
@@ -30,16 +28,7 @@ fn input(
 			velocity.linvel += Vec3::new(-direction.z, 0., direction.x);
 		}
 		if keyboard.just_pressed(KeyCode::Space) {
-			velocity.linvel += Vec3::Y;
+			velocity.linvel += Vec3::Y * 10.;
 		}
-	}
-	for gamepad in gamepads.iter() {
-		let left_stick_x = axes
-			.get(GamepadAxis::new(gamepad, GamepadAxisType::LeftStickX))
-			.unwrap_or_default();
-		let left_stick_y = axes
-			.get(GamepadAxis::new(gamepad, GamepadAxisType::LeftStickY))
-			.unwrap_or_default();
-		let _direction = Vec3::new(left_stick_x, 0., left_stick_y).clamp_length(-speed.0, speed.0);
 	}
 }
